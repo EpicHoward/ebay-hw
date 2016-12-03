1bf265735e7bd46192f64f72b9b27050840b6578
Eric R. Howard <ehoward@hudsonhs.com>
2016-12-03 21:32:07 +0000

    ebay homework

---
+++ code.js
@@ -0,0 +1,28 @@
+// JavaScript File
+
+$(document).ready(function(){
+
+	$("#button").click(function(){
+
+		$("html").append('<img src="http://stream1.gifsoup.com/webroot/animatedgifs4/1273487_o.gif"/>');
+
+
+	});
+
+
+
+});
+
+
+
+
+
+$(document).ready(function(){
+    $("#button").click(function(){
+        $("button").hide();
+        
+    });
+
+
+
+});
